49c0167cce823aa1c73ad9e8164ac2c7e1b9f195
Pragadeeswaran196 <101637224+Pragadeeswaran196@users.noreply.github.com>
2023-08-20 22:29:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ variable "input_file_path" {
 locals {
   Queue = [for line in split("\n", file(var.input_file_path)) : {
     Queue_name = split(":", line)[0]
-    Threshold = split(":", line)[1]
   }]
 }
 
