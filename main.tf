24fa68d5c0b5a3998a9aaeb2f7f36b354996ef7b
Pragadeeswaran196 <101637224+Pragadeeswaran196@users.noreply.github.com>
2023-08-20 22:19:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
+variable "input_file_path" {
+  description = "Path to the input text file"
+  type        = string
+  default     = "input.txt"
+}
+
 locals {
-  Queue = [for line in split("\n", file("input.txt")) : {
+  Queue = [for line in split("\n", file(var.input_file_path)) : {
     Queue_name = split(":", line)[0]
     Threshold = split(":", line)[1]
     sustain = split(":", line)[2]
   }]
+}
+
+output "queue_info" {
+  value = local.Queue
 }
