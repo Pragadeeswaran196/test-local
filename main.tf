1cb27203382e81b4fc6445c63d8af2240d938ba0
Pragadeeswaran196 <101637224+Pragadeeswaran196@users.noreply.github.com>
2023-08-20 22:38:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,13 @@ variable "input_file_path" {
 }
 
 locals {
-  Queue = [for line in split("\n", file(var.input_file_path)) : {
-    Queue_name = split(":", line)[0]
-  }]
+  Queue = [
+    for line in split("\n", file(var.input_file_path)) : {
+      Queue_name = length(split(":", line)) >= 1 ? split(":", line)[0] : ""
+      Threshold  = length(split(":", line)) >= 2 ? split(":", line)[1] : ""
+      sustain    = length(split(":", line)) >= 3 ? split(":", line)[2] : ""
+    }
+  ]
 }
 
 output "queue_info" {
