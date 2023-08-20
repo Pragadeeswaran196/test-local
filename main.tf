4d547b9e8604ed012ff53d4e17fb5d7f3d759cf4
Pragadeeswaran196 <101637224+Pragadeeswaran196@users.noreply.github.com>
2023-08-20 22:17:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 locals {
-  Queue = [for line in split("\n", file("visble_msg.txt")) : {
-    Queue_name = split(",", line)[0]
-    Threshold = split(",", line)[1]
-    sustain = split(",", line)[2]
+  Queue = [for line in split("\n", file("input.txt")) : {
+    Queue_name = split(":", line)[0]
+    Threshold = split(":", line)[1]
+    sustain = split(":", line)[2]
   }]
 }
