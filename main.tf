8e95397814f12c8c091fc7115d1ca58b8717b005
Pragadeeswaran196 <101637224+Pragadeeswaran196@users.noreply.github.com>
2023-08-20 22:15:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,7 @@
-variable "input_file_path" {
-  description = "Path to the input text file"
-  type        = string
-  default     = "input.txt"
-}
-
-data "local_file" "input" {
-  depends_on = [var.input_file_path]
-  filename  = var.input_file_path
-}
-
-output "processed_text" {
-  value = data.local_file.input.content
+locals {
+  Queue = [for line in split("\n", file("visble_msg.txt")) : {
+    Queue_name = split(",", line)[0]
+    Threshold = split(",", line)[1]
+    sustain = split(",", line)[2]
+  }]
 }
